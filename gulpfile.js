4b92268cebf248418568e735aa67af5753e6d89e
lichtle <leticia.negreiros@outlook.com>
2023-10-01 12:45:21 -0300

    fix: added missing scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const sourcemaps = require("gulp-sourcemaps");
 
 function styles() {
   // Compilando e comprimindo o Sass
   return gulp
     .src("./src/styles/*.scss") // Arquivo ou arquivos de origem
+    .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(sourcemaps.write("./maps"))
     .pipe(gulp.dest("./dist/styles")); // Pasta de destino
 }
 
